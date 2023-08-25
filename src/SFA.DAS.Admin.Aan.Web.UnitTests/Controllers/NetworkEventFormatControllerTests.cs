ab3cf5c237162c4b93602820613c7d11073295b3
MarkFCain <mark.cain@digital.education.gov.uk>
2023-08-25 13:36:56 +0100

    CSP-922 coverage added for controller post tests

--- src/SFA.DAS.Admin.Aan.Web.UnitTests/Controllers/NetworkEventFormatControllerTests.cs
+++ src/SFA.DAS.Admin.Aan.Web.UnitTests/Controllers/NetworkEventFormatControllerTests.cs
@@ -1,6 +1,11 @@
 ﻿using AutoFixture.NUnit3;
 using FluentAssertions;
+using FluentValidation;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SFA.DAS.Admin.Aan.Application.Constants;
+using SFA.DAS.Admin.Aan.Application.Services;
 using SFA.DAS.Admin.Aan.Web.Controllers;
 using SFA.DAS.Admin.Aan.Web.Infrastructure;
 using SFA.DAS.Admin.Aan.Web.Models.NetworkEvent;
@@ -19,6 +24,36 @@ public class NetworkEventFormatControllerTests
     {
         sut.AddUrlHelperMock().AddUrlForRoute(RouteNames.NetworkEvents, AllNetworksUrl);
         var result = (ViewResult)sut.Get();
+
+        Assert.That(result.Model, Is.TypeOf<CreateEventFormatViewModel>());
+        var vm = result.Model as CreateEventFormatViewModel;
+        vm!.BackLink.Should().Be(AllNetworksUrl);
+    }
+
+    [TestCase(EventFormat.InPerson)]
+    [TestCase(EventFormat.Hybrid)]
+    [TestCase(EventFormat.Online)]
+    public void Post_SetNullEventFormatOnSessionModel(EventFormat eventFormat)
+    {
+        var sessionServiceMock = new Mock<ISessionService>();
+        var validatorMock = new Mock<IValidator<CreateEventFormatViewModel>>();
+
+        var sessionModel = new CreateEventSessionModel();
+
+        sessionServiceMock.Setup(s => s.Get<CreateEventSessionModel>()).Returns(sessionModel);
+
+        var submitModel = new CreateEventFormatViewModel { EventFormat = eventFormat };
+
+        var validationResult = new ValidationResult();
+        validatorMock.Setup(v => v.Validate(submitModel)).Returns(validationResult);
+
+        var sut = new NetworkEventFormatController(sessionServiceMock.Object, validatorMock.Object);
+
+        sut.AddUrlHelperMock().AddUrlForRoute(RouteNames.NetworkEvents, AllNetworksUrl);
+
+        var result = (ViewResult)sut.Post(submitModel);
+
+        sessionServiceMock.Verify(s => s.Set(It.Is<CreateEventSessionModel>(m => m.EventFormat == eventFormat)));
 
         Assert.That(result.Model, Is.TypeOf<CreateEventFormatViewModel>());
         var vm = result.Model as CreateEventFormatViewModel;
