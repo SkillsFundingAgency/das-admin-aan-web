86abc401cb63302c904ffbf3cb6d1bdf64b80612
MarkFCain <mark.cain@digital.education.gov.uk>
2023-09-04 11:12:15 +0100

    CSP-923 create event description - page 3 of create event

---
+++ src/SFA.DAS.Admin.Aan.Web/Controllers/CreateEvent/NetworkEventDescriptionController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.Admin.Aan.Web.Controllers.CreateEvent;
+public class NetworkEventDescriptionController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ src/SFA.DAS.Admin.Aan.Web/Models/NetworkEvent/CreateEventDescriptionViewModel.cs
@@ -0,0 +1,11 @@
+﻿using SFA.DAS.Admin.Aan.Web.Infrastructure;
+
+namespace SFA.DAS.Admin.Aan.Web.Models.NetworkEvent;
+
+public class CreateEventDescriptionViewModel : IBackLink
+{
+    public string BackLink { get; set; } = null!;
+
+    public string? EventOutline { get; set; }
+    public string? EventSummary { get; set; }
+}
