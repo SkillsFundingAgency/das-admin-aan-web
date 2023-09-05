fc026dcd068376025659e2b2587b5225e7834e7f
James King <hello@jamesmichaelking.com>
2023-09-05 17:03:36 +0100

    CKEditor

---
+++ src/SFA.DAS.Admin.Aan.Web/wwwroot/js/ckeditor/config.js
@@ -0,0 +1,28 @@
+/**
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here.
+	// For complete reference see:
+	// https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
+
+	// The toolbar groups arrangement, optimized for a single toolbar row.
+	config.toolbarGroups = [
+        {name: 'basicstyles', groups: ['basicstyles', 'cleanup']},
+        {name: 'paragraph',   groups: [ 'list' ] },
+        {name: 'links'}
+    ];
+	
+    config.extraPlugins = 'markdown';
+
+	config.skin = 'gds';
+
+	// The default plugins included in the basic setup define some buttons that
+	// are not needed in a basic editor. They are removed here.
+	config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Strike,Subscript,Superscript';
+
+	// Dialog windows are also simplified.
+	config.removeDialogTabs = 'link:advanced';
+};
