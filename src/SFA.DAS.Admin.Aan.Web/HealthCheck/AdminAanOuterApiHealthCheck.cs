b377064a80dc42243fc712c2c82865cbe56d5d73
MarkFCain <mark.cain@digital.education.gov.uk>
2023-10-12 15:01:58 +0100

    CSP-1089 Added ping to admin-aan-web

---
+++ src/SFA.DAS.Admin.Aan.Web/HealthCheck/AdminAanOuterApiHealthCheck.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using SFA.DAS.Admin.Aan.Application.Services;
+
+namespace SFA.DAS.Admin.Aan.Web.HealthCheck;
+
+public class AdminAanOuterApiHealthCheck : IHealthCheck
+{
+    public const string HealthCheckResultDescription = "Admin Aan Outer API Health Check";
+
+    private readonly IOuterApiClient _outerApiClient;
+    private readonly ILogger<AdminAanOuterApiHealthCheck> _logger;
+
+    public AdminAanOuterApiHealthCheck(ILogger<AdminAanOuterApiHealthCheck> logger, IOuterApiClient outerApiClient)
+    {
+        _logger = logger;
+        _outerApiClient = outerApiClient;
+    }
+
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+    {
+        _logger.LogInformation("Admin Aan Outer API pinging call");
+        try
+        {
+            await _outerApiClient.GetCalendars(new CancellationToken());
+            return HealthCheckResult.Healthy(HealthCheckResultDescription);
+
+        }
+        catch (Exception)
+        {
+            _logger.LogError("Apprentice Aan Outer API ping failed");
+            return HealthCheckResult.Unhealthy(HealthCheckResultDescription);
+        }
+    }
+}
