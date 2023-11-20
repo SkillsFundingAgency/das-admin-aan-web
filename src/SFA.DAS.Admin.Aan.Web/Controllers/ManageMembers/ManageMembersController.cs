df6cc513874cb03f2406b06e59f2d94736705976
Deven Shah <devenshah1974@gmail.com>
2023-11-20 10:47:23 +0000

    CSP-1074 Added ManageMembers controller

---
+++ src/SFA.DAS.Admin.Aan.Web/Controllers/ManageMembers/ManageMembersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Admin.Aan.Web.Authentication;
+
+namespace SFA.DAS.Admin.Aan.Web.Controllers.ManageMembers;
+
+[Authorize(Roles = Roles.ManageMembersRole)]
+public class ManageMembersController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
