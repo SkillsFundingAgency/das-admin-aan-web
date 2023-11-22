dcf79a5fb635fb5071b4bec5674f8817ee32287a
Deven Shah <devenshah1974@gmail.com>
2023-11-22 08:39:51 +0000

    CSP-1074 Added MemberSummary outer API response

---
+++ src/SFA.DAS.Admin.Aan.Application.UnitTests/OuterApi/Members/MemberSummaryTests.cs
@@ -0,0 +1,51 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using SFA.DAS.Aan.SharedUi.Constants;
+using SFA.DAS.Aan.SharedUi.Models.NetworkDirectory;
+using SFA.DAS.Admin.Aan.Application.OuterApi.Members;
+
+namespace SFA.DAS.Admin.Aan.Application.UnitTests.OuterApi.Members;
+
+public class MemberSummaryTests
+{
+    [Test, AutoData]
+    public void Operator_ConvertsTo_MembersViewModel(MemberSummary sut)
+    {
+        MembersViewModel model = sut;
+        sut.Should().BeEquivalentTo(model, options => options.ExcludingMissingMembers());
+    }
+
+    [Test]
+    [InlineAutoData(true)]
+    [InlineAutoData(false)]
+    public void Operator_DependingOnMemberIsRegionalChair_PopulatesUserRole(bool isRegionalChair, MemberSummary sut)
+    {
+        sut = sut with { IsRegionalChair = isRegionalChair };
+        MembersViewModel model = sut;
+        if (isRegionalChair)
+        {
+            model.UserRole.Should().Be(Role.RegionalChair);
+        }
+        else
+        {
+            model.UserRole.ToString().Should().Be(sut.UserType.ToString());
+        }
+    }
+
+    [Test]
+    [InlineAutoData(null, "")]
+    [InlineAutoData(1, "regionName")]
+    public void Operator_RegionalIdIsNull_PopulatesUserRole(int? regionId, string regionName, MemberSummary sut)
+    {
+        sut = sut with { RegionId = regionId, RegionName = regionName };
+        MembersViewModel model = sut;
+        if (regionId == null)
+        {
+            model.RegionName.Should().Be(MemberSummary.MultiRegional);
+        }
+        else
+        {
+            model.RegionName.Should().Be(sut.RegionName);
+        }
+    }
+}
--- src/SFA.DAS.Admin.Aan.Application/OuterApi/Members/MemberSummary.cs
+++ src/SFA.DAS.Admin.Aan.Application/OuterApi/Members/MemberSummary.cs
@@ -1,5 +1,22 @@
-﻿using SFA.DAS.Aan.SharedUi.Models.AmbassadorProfile;
+﻿using SFA.DAS.Aan.SharedUi.Constants;
+using SFA.DAS.Aan.SharedUi.Models.AmbassadorProfile;
+using SFA.DAS.Aan.SharedUi.Models.NetworkDirectory;
 
 namespace SFA.DAS.Admin.Aan.Application.OuterApi.Members;
 
-public record MemberSummary(Guid MemberId, string FullName, int? RegionId, string? RegionName, MemberUserType UserType, bool IsRegionalChair, DateTime JoinedDate);
+public record MemberSummary(Guid MemberId, string FullName, int? RegionId, string? RegionName, MemberUserType UserType, bool IsRegionalChair, DateTime JoinedDate)
+{
+    public const string MultiRegional = "Multi-regional";
+
+    public static implicit operator MembersViewModel(MemberSummary source)
+        => new()
+        {
+            MemberId = source.MemberId,
+            FullName = source.FullName,
+            RegionId = source.RegionId,
+            RegionName = (source.RegionId != null) ? source.RegionName : MultiRegional,
+            UserRole = source.IsRegionalChair ? Role.RegionalChair : Enum.Parse<Role>(source.UserType.ToString()),
+            IsRegionalChair = source.IsRegionalChair,
+            JoinedDate = source.JoinedDate
+        };
+}
