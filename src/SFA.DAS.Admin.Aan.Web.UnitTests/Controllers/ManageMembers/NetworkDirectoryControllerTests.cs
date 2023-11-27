f9d50235acce72d227ea151de75bcb835a679a5b
Deven Shah <devenshah1974@gmail.com>
2023-11-27 16:23:51 +0000

    CSP-1074 Implements the manage ambassadors filters and result

---
+++ src/SFA.DAS.Admin.Aan.Web.UnitTests/Controllers/ManageMembers/NetworkDirectoryControllerTests.cs
@@ -0,0 +1,146 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SFA.DAS.Aan.SharedUi.Constants;
+using SFA.DAS.Aan.SharedUi.Infrastructure;
+using SFA.DAS.Aan.SharedUi.Models.NetworkDirectory;
+using SFA.DAS.Admin.Aan.Application.OuterApi.Members;
+using SFA.DAS.Admin.Aan.Application.OuterApi.Regions;
+using SFA.DAS.Admin.Aan.Application.Services;
+using SFA.DAS.Admin.Aan.Web.Controllers.ManageMembers;
+using SFA.DAS.Admin.Aan.Web.UnitTests.TestHelpers;
+
+namespace SFA.DAS.Admin.Aan.Web.UnitTests.Controllers.ManageMembers;
+
+public class NetworkDirectoryControllerTests
+{
+    private NetworkDirectoryRequestModel _requestModel = null!;
+    private IActionResult _actualResult = null!;
+    private GetMembersResponse _getMembersResponse = null!;
+    private GetRegionsResult _getRegionsResponse = null!;
+    private List<Region> _regionsData = null!;
+    private const int RegionsCount = 10;
+    private string _networkDirectoryUrl = null!;
+
+    [SetUp]
+    public async Task Init()
+    {
+        Fixture fixture = new();
+        var cancellationToken = fixture.Create<CancellationToken>();
+
+        Mock<IOuterApiClient> apiClientMock = new();
+
+        _getMembersResponse = fixture.Create<GetMembersResponse>();
+        apiClientMock.Setup(c => c.GetMembers(It.IsAny<Dictionary<string, string[]>>(), cancellationToken)).ReturnsAsync(_getMembersResponse);
+
+        var sequence = new Queue<int>(Enumerable.Range(1, RegionsCount));
+        _regionsData = fixture.Build<Region>().With(r => r.Ordering, () => sequence.Dequeue()).CreateMany(RegionsCount).ToList();
+        _getRegionsResponse = fixture.Build<GetRegionsResult>().With(r => r.Regions, _regionsData).Create();
+        apiClientMock.Setup(c => c.GetRegions(cancellationToken)).ReturnsAsync(_getRegionsResponse);
+
+        NetworkDirectoryController sut = new(apiClientMock.Object);
+        _networkDirectoryUrl = fixture.Create<string>();
+        sut.AddUrlHelperMock().AddUrlForRoute(SharedRouteNames.NetworkDirectory, _networkDirectoryUrl);
+
+        _requestModel = fixture.Build<NetworkDirectoryRequestModel>()
+            .With(m => m.RegionId, _regionsData.Take(2).Select(r => r.Id).ToList())
+            .With(m => m.UserRole, new List<Role>() { Role.Apprentice })
+            .Create();
+
+        _actualResult = await sut.Index(_requestModel, cancellationToken);
+    }
+
+    [Test]
+    public void ThenReturnsViewResult()
+    {
+        _actualResult.Should().BeOfType<ViewResult>();
+    }
+
+    [Test]
+    public void ThenReturnsNetworkDirectoryViewModel()
+    {
+        _actualResult.As<ViewResult>().Model.Should().BeOfType<NetworkDirectoryViewModel>();
+        _actualResult.As<ViewResult>().Model.As<NetworkDirectoryViewModel>().TotalCount.Should().Be(_getMembersResponse.TotalCount);
+    }
+
+    [Test]
+    public void ThenReturnsNetworkDirectoryViewModelWithMembers()
+    {
+        _actualResult.As<ViewResult>().Model.Should().BeOfType<NetworkDirectoryViewModel>();
+        _actualResult.As<ViewResult>().Model.As<NetworkDirectoryViewModel>().Members.Count.Should().Be(_getMembersResponse.Members.Count());
+        _actualResult.As<ViewResult>().Model.As<NetworkDirectoryViewModel>().Members.Should().Contain(m => m.MemberProfileLink == "#");
+    }
+
+    [Test]
+    public void ThenNetworkDirectoryViewModelHasPaginationViewModel()
+    {
+        var model = _actualResult.As<ViewResult>().Model.As<NetworkDirectoryViewModel>();
+
+        using (new AssertionScope())
+        {
+            model.PaginationViewModel.Should().NotBeNull();
+            model.PaginationViewModel.CurrentPage.Should().Be(_getMembersResponse.Page);
+            model.PaginationViewModel.PageSize.Should().Be(_getMembersResponse.PageSize);
+            model.PaginationViewModel.TotalPages.Should().Be(_getMembersResponse.TotalPages);
+        }
+    }
+
+    [Test]
+    public void ThenNetworkDirectoryViewModelHasDirectoryFilterChoices()
+    {
+        var model = _actualResult.As<ViewResult>().Model.As<NetworkDirectoryViewModel>();
+
+        using (new AssertionScope("FilterChoices"))
+        {
+            model.FilterChoices.Should().NotBeNull();
+            model.FilterChoices.Keyword.Should().Be(_requestModel.Keyword?.Trim());
+        }
+    }
+
+    [Test]
+    public void ThenNetworkDirectoryViewModelHasRoleFilterChoices()
+    {
+        var model = _actualResult.As<ViewResult>().Model.As<NetworkDirectoryViewModel>();
+        using (new AssertionScope("RoleCheckList"))
+        {
+            model.FilterChoices.RoleChecklistDetails.Title.Should().Be(NetworkDirectoryController.RoleCheckListTitle);
+            model.FilterChoices.RoleChecklistDetails.QueryStringParameterName.Should().Be(NetworkDirectoryController.RoleCheckListQueryParameterName);
+            model.FilterChoices.RoleChecklistDetails.Lookups.Count().Should().Be(3);
+            model.FilterChoices.RoleChecklistDetails.Lookups.Should().Contain(l => l.Value == nameof(Role.Apprentice) && l.Checked.Equals("checked"));
+            model.FilterChoices.RoleChecklistDetails.Lookups.Should().Contain(l => l.Value == nameof(Role.Employer) && l.Checked.Equals(string.Empty));
+            model.FilterChoices.RoleChecklistDetails.Lookups.Should().Contain(l => l.Value == nameof(Role.RegionalChair) && l.Checked.Equals(string.Empty));
+        }
+    }
+
+    [Test]
+    public void ThenNetworkDirectoryViewModelHasRegionFilterChoices()
+    {
+        var model = _actualResult.As<ViewResult>().Model.As<NetworkDirectoryViewModel>();
+        using (new AssertionScope("RegionsCheckList"))
+        {
+            model.FilterChoices.RegionChecklistDetails.Title.Should().Be(NetworkDirectoryController.RegionCheckListTitle);
+            model.FilterChoices.RegionChecklistDetails.QueryStringParameterName.Should().Be(NetworkDirectoryController.RegionCheckListParameterName);
+            model.FilterChoices.RegionChecklistDetails.Lookups.Count().Should().Be(RegionsCount + 1);
+            model.FilterChoices.RegionChecklistDetails.Lookups.Where(l => l.Checked == "checked").Count().Should().Be(2);
+            model.FilterChoices.RegionChecklistDetails.Lookups.Where(l => l.Checked == string.Empty).Count().Should().Be(9);
+            model.FilterChoices.RegionChecklistDetails.Lookups.Should().Contain(r => r.Value == "0" && r.Name == "Multi-regional");
+        }
+    }
+
+    [Test]
+    public void ThenNetworkDirectoryViewModelHasSelectedFilters()
+    {
+        var model = _actualResult.As<ViewResult>().Model.As<NetworkDirectoryViewModel>();
+
+        using (new AssertionScope("Selected filters"))
+        {
+            model.SelectedFiltersModel.SelectedFilters.Should().NotBeNullOrEmpty();
+            model.SelectedFiltersModel.SelectedFilters.Should().ContainSingle(s => s.FieldName == "Role" && s.Filters.Any(f => f.Value == "Apprentice"));
+            model.SelectedFiltersModel.SelectedFilters.Should().ContainSingle(s => s.FieldName == "Network directory" && s.Filters.Any(f => f.Value == _requestModel.Keyword));
+            model.SelectedFiltersModel.SelectedFilters.Should().ContainSingle(s => s.FieldName == "Region" && s.Filters.Count == 2);
+            model.SelectedFiltersModel.ClearSelectedFiltersLink.Should().Be(_networkDirectoryUrl);
+        }
+    }
+}
---
+++ src/SFA.DAS.Admin.Aan.Web/Controllers/ManageMembers/NetworkDirectoryController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Aan.SharedUi.Constants;
+using SFA.DAS.Aan.SharedUi.Extensions;
+using SFA.DAS.Aan.SharedUi.Infrastructure;
+using SFA.DAS.Aan.SharedUi.Models;
+using SFA.DAS.Aan.SharedUi.Models.NetworkDirectory;
+using SFA.DAS.Aan.SharedUi.Models.NetworkEvents;
+using SFA.DAS.Aan.SharedUi.Services;
+using SFA.DAS.Admin.Aan.Application.OuterApi.Members;
+using SFA.DAS.Admin.Aan.Application.OuterApi.Regions;
+using SFA.DAS.Admin.Aan.Application.Services;
+using SFA.DAS.Admin.Aan.Web.Authentication;
+using SFA.DAS.ApprenticeAan.Web.Models.NetworkDirectory;
+
+namespace SFA.DAS.Admin.Aan.Web.Controllers.ManageMembers;
+
+[Authorize(Roles = Roles.ManageMembersRole)]
+[Route("network-directory", Name = SharedRouteNames.NetworkDirectory)]
+public class NetworkDirectoryController : Controller
+{
+    public const string RoleCheckListTitle = "Role";
+    public const string RoleCheckListQueryParameterName = "userRole";
+    public const string RegionCheckListTitle = "Region";
+    public const string RegionCheckListParameterName = "regionId";
+    private readonly IOuterApiClient _outerApiClient;
+
+    public NetworkDirectoryController(IOuterApiClient outerApiClient)
+    {
+        _outerApiClient = outerApiClient;
+    }
+
+    public async Task<IActionResult> Index(NetworkDirectoryRequestModel request, CancellationToken cancellationToken)
+    {
+        var getMembersTask = _outerApiClient.GetMembers(request.ToQueryStringParameters(), cancellationToken);
+        var regionTask = _outerApiClient.GetRegions(cancellationToken);
+
+        await Task.WhenAll(getMembersTask, regionTask);
+
+        var regions = regionTask.Result.Regions;
+        regions.Add(new Region(0, "Multi-regional", regions.Count + 1));
+
+        var model = InitialiseViewModel(getMembersTask.Result);
+
+        var filterUrl = FilterBuilder.BuildFullQueryString(request, () => Url.RouteUrl(SharedRouteNames.NetworkDirectory)!);
+
+        model.PaginationViewModel = new PaginationViewModel(getMembersTask.Result.Page, getMembersTask.Result.PageSize, getMembersTask.Result.TotalPages, filterUrl);
+
+        var filterChoices = PopulateFilterChoices(request, regions);
+        model.FilterChoices = filterChoices;
+        model.SelectedFiltersModel.SelectedFilters = FilterBuilder.Build(request, () => Url.RouteUrl(SharedRouteNames.NetworkDirectory)!, filterChoices.RoleChecklistDetails.Lookups, filterChoices.RegionChecklistDetails.Lookups);
+        model.SelectedFiltersModel.ClearSelectedFiltersLink = Url.RouteUrl(SharedRouteNames.NetworkDirectory)!;
+
+        return View(model);
+    }
+
+    private static NetworkDirectoryViewModel InitialiseViewModel(GetMembersResponse result)
+    {
+        var model = new NetworkDirectoryViewModel
+        {
+            TotalCount = result.TotalCount
+        };
+
+        foreach (var member in result.Members)
+        {
+            MembersViewModel vm = member;
+            vm.MemberProfileLink = "#";
+            model.Members.Add(vm);
+        }
+
+        return model;
+    }
+
+    private static DirectoryFilterChoices PopulateFilterChoices(NetworkDirectoryRequestModel request, List<Region> regions)
+        => new()
+        {
+            Keyword = request.Keyword?.Trim(),
+            RoleChecklistDetails = new ChecklistDetails
+            {
+                Title = RoleCheckListTitle,
+                QueryStringParameterName = RoleCheckListQueryParameterName,
+                Lookups = new ChecklistLookup[]
+                {
+                    new(Role.Apprentice.GetDescription(), Role.Apprentice.ToString(), request.UserRole.Exists(x => x == Role.Apprentice)),
+                    new(Role.Employer.GetDescription(), Role.Employer.ToString(), request.UserRole.Exists(x => x == Role.Employer)),
+                    new(Role.RegionalChair.GetDescription(), Role.RegionalChair.ToString(), request.UserRole.Exists(x => x == Role.RegionalChair))
+                }
+            },
+            RegionChecklistDetails = new ChecklistDetails
+            {
+                Title = RegionCheckListTitle,
+                QueryStringParameterName = RegionCheckListParameterName,
+                Lookups = regions.OrderBy(x => x.Ordering).Select(region => new ChecklistLookup(region.Area, region.Id.ToString(), request.RegionId.Exists(x => x == region.Id))).ToList()
+            }
+        };
+}
