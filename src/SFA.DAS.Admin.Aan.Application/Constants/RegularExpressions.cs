b0da54be2080e1b033066d05ae65045bdd667eef
Deven Shah <devenshah1974@gmail.com>
2023-11-29 13:34:45 +0000

    CSP-1087 Fix url regex to allow hyphen in domain

--- src/SFA.DAS.Admin.Aan.Application/Constants/RegularExpressions.cs
+++ src/SFA.DAS.Admin.Aan.Application/Constants/RegularExpressions.cs
@@ -4,5 +4,5 @@ public static class RegularExpressions
     public const string ExcludedCharactersRegex = @"^[^@#$^=+\\\/<>%]*$";
     public const string ExcludedCharactersInMarkdownRegex = @"^[^<>]*$";
     public const string EmailRegex = @"^[-!#$%&'*+/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z{|}~])*@[a-zA-Z0-9_](-?[a-zA-Z0-9_])*(\.[a-zA-Z0-9](-?[a-zA-Z0-9])*)+$";
-    public const string UrlRegex = @"(?i)^https:\/\/[(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)";
+    public const string UrlRegex = @"(?i)^https:\/\/[(www\.)?a-zA-Z0-9@:%._\+~#=-]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)";
 }
