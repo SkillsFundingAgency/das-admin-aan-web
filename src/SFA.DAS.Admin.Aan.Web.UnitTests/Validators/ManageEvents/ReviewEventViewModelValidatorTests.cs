f76a7209b92aa1cf164d46e2a26e84416f2a221f
MarkFCain <mark.cain@digital.education.gov.uk>
2023-12-07 16:00:03 +0000

    CSP-1069 validation tests added for review event link

--- src/SFA.DAS.Admin.Aan.Web.UnitTests/Validators/ManageEvents/ReviewEventViewModelValidatorTests.cs
+++ src/SFA.DAS.Admin.Aan.Web.UnitTests/Validators/ManageEvents/ReviewEventViewModelValidatorTests.cs
@@ -107,4 +107,49 @@ public class ReviewEventViewModelValidatorTests
 
         return vm;
     }
+
+    [TestCase(null, null, true)]
+    [TestCase(null, EventFormat.Hybrid, true)]
+    [TestCase(null, EventFormat.InPerson, true)]
+    [TestCase(null, EventFormat.Online, true)]
+    [TestCase("", null, true)]
+    [TestCase("", EventFormat.Hybrid, true)]
+    [TestCase("", EventFormat.InPerson, true)]
+    [TestCase("", EventFormat.Online, true)]
+    [TestCase("notAnUrl", null, true)]
+    [TestCase("notAnUrl", EventFormat.Hybrid, false)]
+    [TestCase("notAnUrl", EventFormat.InPerson, true)]
+    [TestCase("notAnUrl", EventFormat.Online, false)]
+    [TestCase("http://notsecureurl.com", EventFormat.Online, false)]
+    [TestCase("http://notsecureurl.com", EventFormat.Hybrid, false)]
+    [TestCase("https://secureurl.com", EventFormat.Online, true)]
+    [TestCase("https://secureurl.com", EventFormat.Hybrid, true)]
+    [TestCase("HTTPS://secureurl.com", EventFormat.Hybrid, true)]
+    [TestCase("Https://secureurl.com", EventFormat.Hybrid, true)]
+    [TestCase("hTtps://secureurl.com", EventFormat.Hybrid, true)]
+    [TestCase("htTps://secureurl.com", EventFormat.Hybrid, true)]
+    [TestCase("httPs://secureurl.com", EventFormat.Hybrid, true)]
+    [TestCase("httpS://secureurl.com", EventFormat.Hybrid, true)]
+    [TestCase("https://www.secureurl.com", EventFormat.Hybrid, true)]
+    [TestCase("tts://w.something", EventFormat.Hybrid, false)]
+    [TestCase("https://teams.microsoft.com", EventFormat.Online, true)]
+    [TestCase("https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZGRmZC00NzlkLWFmZTktOTU4ZmFkMjA2ZDE1%thread.v2/0?context=%7b%22ad277c9-c60a-4da1-b5f3-b3b8Oid%22%3a%2209a4-c6-48-bc-ad60%22%7d", EventFormat.Hybrid, true)]
+    [TestCase("https://test-install.blindsidenetworks.com/bigbluebutton/api/join?meetingID=DemoMeeting", EventFormat.Online, true)]
+    public void Validate_EventLink(string? url, EventFormat? eventFormat, bool isValid)
+    {
+        var model = new ReviewEventViewModel
+        { EventLocation = "xyz", EventFormat = eventFormat, OnlineEventLink = url };
+
+        var sut = new ReviewEventViewModelValidator();
+        var result = sut.TestValidate(model);
+        if (!isValid)
+        {
+            result.ShouldHaveValidationErrorFor(c => c.OnlineEventLink)
+                .WithErrorMessage(ReviewEventViewModelValidator.EventLinkMustBeValid);
+        }
+        else
+        {
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
 }
