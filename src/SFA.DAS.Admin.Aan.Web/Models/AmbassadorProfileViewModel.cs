e2722df8dd5b51c46422b1e8a6690f0b65b77437
Deven Shah <devenshah1974@gmail.com>
2023-12-18 17:39:17 +0000

    CSP-1088 Integrated modular profile view

---
+++ src/SFA.DAS.Admin.Aan.Web/Models/AmbassadorProfileViewModel.cs
@@ -0,0 +1,17 @@
+﻿using SFA.DAS.Aan.SharedUi.Models.PublicProfile;
+
+namespace SFA.DAS.Admin.Aan.Web.Models;
+
+public class AmbassadorProfileViewModel
+{
+    public ConnectViaLinkedInViewModel ConnectViaLinkedIn { get; set; } = new ConnectViaLinkedInViewModel();
+
+
+    public MemberInformationSectionViewModel MemberInformation { get; set; } = new MemberInformationSectionViewModel();
+
+
+    public ApprenticeshipSectionViewModel ApprenticeshipInformation { get; set; } = new ApprenticeshipSectionViewModel();
+
+
+    public AreasOfInterestSectionViewModel AreasOfInterest { get; set; } = new AreasOfInterestSectionViewModel();
+}
