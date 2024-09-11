32469b4952a57d0de1ec0b5dae36e5d3a2e165e6
Chris Foster <chrisfoster186@googlemail.com>
2024-09-11 11:31:06 +0100

    Adds notification settings option to aan admin hub page

--- src/SFA.DAS.Admin.Aan.Web/Models/AdminHubViewModel.cs
+++ src/SFA.DAS.Admin.Aan.Web/Models/AdminHubViewModel.cs
@@ -1,3 +1,3 @@
 ﻿namespace SFA.DAS.Admin.Aan.Web.Models;
 
-public record AdminHubViewModel(bool HasManageEventsRole, bool HasManageMembersRole, string ManageEventsUrl, string ManageAmbassadorsUrl);
+public record AdminHubViewModel(bool HasManageEventsRole, bool HasManageMembersRole, string ManageEventsUrl, string ManageAmbassadorsUrl, string NotificationSettingsUrl);
