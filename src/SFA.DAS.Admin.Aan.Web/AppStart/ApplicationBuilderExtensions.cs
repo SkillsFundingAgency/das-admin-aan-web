1b2a1a3ffba1a9b883534539fdd0449d4a26933a
ratheeshri1984 <ratheeshri1984@gmail.com>
2024-11-20 15:00:19 +0000

    EC-799-Xss Fix

---
+++ src/SFA.DAS.Admin.Aan.Web/AppStart/ApplicationBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Polly;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SFA.DAS.Admin.Aan.Web.AppStart;
+
+[ExcludeFromCodeCoverage]
+public static class ApplicationBuilderExtensions
+{
+    public static IApplicationBuilder UseContentSecurityPolicy(this IApplicationBuilder app)
+    {
+        const string dasCdn = "das-at-frnt-end.azureedge.net das-pp-frnt-end.azureedge.net das-mo-frnt-end.azureedge.net das-test-frnt-end.azureedge.net das-test2-frnt-end.azureedge.net das-prd-frnt-end.azureedge.net";
+        app.Use(async (context, next) =>
+        {
+            var nonce = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+            context.Items["CspNonce"] = nonce;
+
+            context.Response.Headers["Content-Security-Policy"] =
+                $"script-src 'self' 'nonce-{nonce}' {dasCdn} *.tagmanager.google.com https://ssl.google-analytics.com *.googletagmanager.com *.google-analytics.com *.googleapis.com https://*.services.visualstudio.com https://*.rcrsv.io; " +
+                $"style-src 'self' 'unsafe-inline' {dasCdn} *.tagmanager.google.com https://fonts.googleapis.com https://*.rcrsv.io; " +
+                $"img-src {dasCdn} *.googletagmanager.com https://ssl.gstatic.com https://www.gstatic.com *.google-analytics.com https://*.rcrsv.io; " +
+                $"font-src {dasCdn} https://fonts.gstatic.com https://*.rcrsv.io; " +
+                "connect-src 'self' *.google-analytics.com https://*.rcrsv.io; " +
+                "frame-src *.googletagmanager.com https://*.rcrsv.io";
+
+            await next();
+        });
+
+        return app;
+    }
+}
